7e2a83db5315e2bda56441af89c8dc7fc46a56bc
Рауф <rauf@MacBook-Pro-Rauf.local>
2023-10-18 17:52:21 +0600

    first

---
+++ phonebook/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from . import models
+
+class CreatePersoneForm(forms.ModelForm):
+    phones = forms.CharField(widget=forms.Textarea(), help_text="separated by new line '\\n' ")
+    class Meta:
+        model=models.Persone
+        fields=('name','phones')
